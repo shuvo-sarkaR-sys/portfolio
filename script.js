e1fbd0c3ab41e51c6f15cce3d6a042c34331a9f2
shuvo-sarkaR-sys <md.sirforce@gmail.com>
2025-09-12 11:35:02 +0600

    upd

--- script.js
+++ script.js
@@ -2,3 +2,38 @@
       document.getElementById("fadeBtn").classList.add("show");
       document.querySelector(".profile").classList.add("show");
     };
+ const btn1 = document.getElementById("btn-1");
+  const btn2 = document.getElementById("btn-2");
+  const img1 = document.getElementById("inner-box");
+  const img2 = document.getElementById("inner-box-1");
+
+  btn1.addEventListener("click", () => {
+    img1.classList.remove("hidden");
+    img2.classList.add("hidden");
+
+    btn1.classList.add("bg-fuchsia-600", "text-white");
+    btn2.classList.remove("bg-fuchsia-600", "text-white");
+  });
+
+  btn2.addEventListener("click", () => {
+    img2.classList.remove("hidden");
+    img2.classList.add("flex");
+    img1.classList.add("hidden");
+
+    btn2.classList.add("bg-fuchsia-600", "text-white");
+    btn1.classList.remove("bg-fuchsia-600", "text-white");
+  });
+   const swiper = new Swiper(".mySwiper", {
+      loop: true,
+      autoplay: {
+        delay: 3000,
+      },
+      pagination: {
+        el: ".custom-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+          const labels = ["Restaurant Website", "Ecommerce Website", "Dashboard"];
+          return `<button class="${className}">${labels[index]}</button>`;
+        },
+      },
+    });
